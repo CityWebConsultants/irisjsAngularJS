c99871472e22359a4367960e2f2ac8a5cae85d92
Adam Clarey <adam@citywebconsultants.co.uk>
2016-06-09 16:31:56 +0100

    add js to page

--- irisjsAngularJS.js
+++ irisjsAngularJS.js
@@ -3,31 +3,36 @@
  * Load in necessary JavaScript files on the client side if entity embed is present
  */
 
-iris.modules.irisjsAngularJS.registerHook("hook_frontend_embed__entity", 1, function (thisHook, data) {
+iris.modules.irisjsAngularJS.registerHook("hook_frontend_template", 2, function (thisHook, data) {
 
-  thisHook.context.vars.tags.headTags["socket.io"] = {
-    type: "script",
-    attributes: {
-      "src": "/socket.io/socket.io.js"
-    },
-    rank: -1
+  if (!thisHook.context.vars) {
+
+    thisHook.context.vars = { 'tags' : { 'headTags' : {}}};
   }
 
-  thisHook.context.vars.tags.headTags["angular"] = {
-    type: "script",
-    attributes: {
-      "src": "https://ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular.js"
-    },
-    rank: 0
-  }
+    thisHook.context.vars.tags.headTags["socket.io"] = {
+      type: "script",
+      attributes: {
+        "src": "/socket.io/socket.io.js"
+      },
+      rank: -1
+    }
 
-  thisHook.context.vars.tags.headTags["irisjsAngularJS"] = {
-    type: "script",
-    attributes: {
-      "src": "/modules/irisjsAngularJS/irisjsAngularJS.js"
-    },
-    rank: 1
-  }
+    thisHook.context.vars.tags.headTags["angular"] = {
+      type: "script",
+      attributes: {
+        "src": "https://ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular.js"
+      },
+      rank: 0
+    }
+
+    thisHook.context.vars.tags.headTags["irisjsAngularJS"] = {
+      type: "script",
+      attributes: {
+        "src": "/modules/irisjsAngularJS/irisjsAngularJS.js"
+      },
+      rank: 1
+    }
 
   thisHook.pass(data);
 
